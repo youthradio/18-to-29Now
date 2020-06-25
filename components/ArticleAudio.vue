024bd15a1bcb6c21e48328174b238ceaf30eee20
Radames Ajna <radamajna@gmail.com>
2020-06-25 11:09:17 -0700

    feature image on audio article

--- components/ArticleAudio.vue
+++ components/ArticleAudio.vue
@@ -11,9 +11,9 @@
     <h1 class="serif blue f3 f2-ns lh-title mb0 ttc">
       {{ article.title }}
     </h1>
-    <div class="flex flex-column flex-row-ns justify-between">
+    <div class="flex flex-column flex-row-ns justify-between mt2">
       <div class="w-20-ns">
-        <h5 class="dark-red lh-title pr3-ns normal mb0">PRODUCED BY</h5>
+        <h5 class="dark-red lh-title pr3-ns normal mv0">PRODUCED BY</h5>
         <h3 class="dark-red lh-title pr3-ns mt1 mb1">
           {{ article.author }}
         </h3>
@@ -25,10 +25,16 @@
         </h5>
       </div>
 
-      <p class="dn db-ns f4 lh-copy order-2 w-20-ns pl3-ns">
+      <p class="dn db-ns lh-copy order-2 w-20-ns pl3-ns mt0">
         {{ article.summary }}
       </p>
       <div class="measure-wide ph3-ns w-60-ns lh-copy center f5 f4-ns">
+        <img
+          :data-src="article.featureImage"
+          src="blankfeature.jpg"
+          class="db lazyload"
+          alt="Photo of a dimly lit room with a computer interface terminal."
+        />
         <article v-html="article.text"></article>
       </div>
     </div>
