d559d67bd53fb1da70713944367f8a9aca5f81c8
Radames Ajna <radamajna@gmail.com>
2020-07-27 15:06:35 -0700

    create paginator component bootstrap

---
+++ components/PaginatorContainer.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="mw8 center ph3 relative z-1 mt2">
+    <template v-for="article in articles">
+      <article :key="article.slug" class="pb4">
+        <div class="flex flex-column flex-row-ns">
+          <div class="pr3-ns mb4 mb0-ns w-30-ns">
+            <nuxt-link
+              :title="article.title"
+              :to="{ path: `story/${article.slug}` }"
+              class="link db dim black"
+            >
+              <img
+                :data-src="article.featureImageSmall"
+                class="db lazyload"
+                src="blankfeature.jpg"
+                style="object-fit: cover;"
+                alt="Photo of a dimly lit room with a computer interface terminal."
+              />
+            </nuxt-link>
+          </div>
+          <div class="w-70-ns pl3-ns">
+            <nuxt-link
+              :title="article.title"
+              :to="{ path: `story/${article.slug}` }"
+              class="link db blue underline-hover"
+            >
+              <h2 class="blue serif mv0 lh-title f4 f3-ns">
+                {{ article.title }}
+              </h2>
+            </nuxt-link>
+            <a
+              :title="`${article.author} Bio`"
+              class="pointer link db blue dim black"
+              @click.prevent="$emit('toggleBioModalSlug', article.authorslug)"
+            >
+              <h3 class="dark-red lh-title mv1 f5 f4-ns">
+                {{ article.author }} |
+                <span class="ttc normal"> {{ article.format }}</span>
+              </h3>
+            </a>
+            <nuxt-link
+              :title="article.summary"
+              :to="{ path: `story/${article.slug}` }"
+              class="link db dim black"
+            >
+              <p class="f4-l lh-copy mv0">{{ article.summary }}</p>
+            </nuxt-link>
+          </div>
+        </div>
+      </article>
+    </template>
+  </section>
+</template>
+<script>
+import mixinMethods from '~/utils/mixinMethods'
+
+export default {
+  mixins: [mixinMethods],
+  props: {
+    articles: {
+      type: Array,
+      required: true
+    }
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -14,57 +14,11 @@
         class="ph3 relative z-1"
         @toggleBioModalSlug="(authorslug) => toggleBioModalSlug(authorslug)"
       />
-      <section class="mw8 center ph3 relative z-1 mt2">
-        <template v-for="article in latest">
-          <article :key="article.slug" class="pb4">
-            <div class="flex flex-column flex-row-ns">
-              <div class="pr3-ns mb4 mb0-ns w-30-ns">
-                <nuxt-link
-                  :title="article.title"
-                  :to="{ path: `story/${article.slug}` }"
-                  class="link db dim black"
-                >
-                  <img
-                    :data-src="article.featureImageSmall"
-                    class="db lazyload"
-                    src="blankfeature.jpg"
-                    style="object-fit: cover;"
-                    alt="Photo of a dimly lit room with a computer interface terminal."
-                  />
-                </nuxt-link>
-              </div>
-              <div class="w-70-ns pl3-ns">
-                <nuxt-link
-                  :title="article.title"
-                  :to="{ path: `story/${article.slug}` }"
-                  class="link db blue underline-hover"
-                >
-                  <h2 class="blue serif mv0 lh-title f4 f3-ns">
-                    {{ article.title }}
-                  </h2>
-                </nuxt-link>
-                <a
-                  :title="`${article.author} Bio`"
-                  class="pointer link db blue dim black"
-                  @click.prevent="toggleBioModalSlug(article.authorslug)"
-                >
-                  <h3 class="dark-red lh-title mv1 f5 f4-ns">
-                    {{ article.author }} |
-                    <span class="ttc normal"> {{ article.format }}</span>
-                  </h3>
-                </a>
-                <nuxt-link
-                  :title="article.summary"
-                  :to="{ path: `story/${article.slug}` }"
-                  class="link db dim black"
-                >
-                  <p class="f4-l lh-copy mv0">{{ article.summary }}</p>
-                </nuxt-link>
-              </div>
-            </div>
-          </article>
-        </template>
-      </section>
+      <PaginatorContainer
+        :articles="latest"
+        class="ph3 relative z-1"
+        @toggleBioModalSlug="(authorslug) => toggleBioModalSlug(authorslug)"
+      />
       <div class="bg-washed-red relative z--1">
         <blockquote
           class="flex flex-column flex-row-ns items-end f3 f2-ns center mw8 ph3"
@@ -158,12 +112,14 @@ import mixinMethods from '~/utils/mixinMethods'
 import MainHeader from '~/components/MainHeader.vue'
 import Footer from '~/components/Footer.vue'
 import HorizontalContainer from '~/components/HorizontalContainer.vue'
+import PaginatorContainer from '~/components/PaginatorContainer.vue'
 
 export default {
   components: {
     MainHeader,
     Footer,
-    HorizontalContainer
+    HorizontalContainer,
+    PaginatorContainer
   },
   mixins: [mixinMethods],
   asyncData(ctx) {
