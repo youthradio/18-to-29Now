27374ecd9a68ca7bbb9b10344d0ba19864f668ea
Radames Ajna <radamajna@gmail.com>
2020-11-17 11:40:59 -0800

    fix preload css

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,7 +110,7 @@ export default {
       {
         href:
           'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800&display=swap',
-        rel: 'preload',
+        rel: 'preload stylesheet',
         as: 'style',
         onload: "this.onload=null;this.rel='stylesheet'"
       }
